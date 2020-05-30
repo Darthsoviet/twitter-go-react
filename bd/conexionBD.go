3f6c58b80f965e5d5bed433de57b850510408720
Darthsoviet <31465914+Darthsoviet@users.noreply.github.com>
2020-05-30 01:14:00 -0500

    se agrego conexion a mongo

---
+++ bd/conexionBD.go
@@ -0,0 +1,40 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//MongoCN es el cliente de conexion a la base de datos
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://root:admin@cluster0-7jbru.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&ssl=true")
+
+/*ConectarBD es la funcion que genera la conexion*/
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Panicln("conexion exitosa con la bd")
+	return client
+}
+
+/*ChequeoConnection se encarga de confirmar si hay una conexion a la base de datos */
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+
+	}
+	return 1
+
+}
--- go.mod
+++ go.mod
@@ -1,7 +1,11 @@
 module https://github.com/Darthsoviet/twitter-go-react
 
-go 1.14.3
+go 1.14
 
-require(
-
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gorilla/mux v1.7.4 // indirect
+	github.com/rs/cors v1.7.0 // indirect
+	go.mongodb.org/mongo-driver v1.3.3 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
 )
